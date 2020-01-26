cbcd41e36735b65ac95b2135be2a86c3e8f83f8a
Colm Higgins <G00287906@gmit.ie>
2020-01-26 19:33:49 +0000

    BMI Calculation for week 2 programming

---
+++ calcBMI.py
@@ -0,0 +1,16 @@
+# This will be a calculation of BMI for a certain individual
+
+#For anyone that wants to input their own weight use below code and remove comments
+#weight = float (input("Enter weight:"))
+#height = float (input("Enter height:"))
+
+#weight in kg
+weight = 65
+#height in cm
+height = 180
+
+BMI = weight / ((height/100)**2)
+#Rounding BMI to decimal place
+acBMI = round(BMI,2)
+
+print (acBMI,"kg/m2")
